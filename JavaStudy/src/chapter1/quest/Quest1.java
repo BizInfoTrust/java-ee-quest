de5976fd49f3568ed1adbb44dbbd358b619b2dac
evochko <evochko@gmail.com>
2015-01-23 19:05:14 +0200

    test

---
+++ JavaStudy/src/chapter1/quest/Quest1.java
@@ -0,0 +1,7 @@
+package chapter1.quest;
+
+class Quest1 { 
+	public static void main (String[] a) { 
+		System.out.println("Java 2");      
+	}  
+} 
---
+++ JavaStudy/src/chapter1/quest/Quest3.java
@@ -0,0 +1,10 @@
+package chapter1.quest;
+
+public class Quest3 {
+
+	public static void main(String s[ ]) { 
+		String args = ""; 
+		System.out.print(args + s); 
+	}  
+
+}
